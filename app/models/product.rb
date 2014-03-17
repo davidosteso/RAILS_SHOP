cf304784e08c9071d66a2aad5f93f97903b5727c
davidosteso <davidosteso@hotmail.com>
2014-03-17 15:26:17 +0100

    PRACTICA 3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+  has_many :line_item
+  has_many :order, :through => :line_item
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304113258) do
+ActiveRecord::Schema.define(version: 20140317092931) do
+
+  create_table "line_items", force: true do |t|
+    t.integer  "order_id"
+    t.integer  "product_id"
+    t.integer  "cantidad"
+    t.decimal  "precio",     precision: 10, scale: 0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "line_items", ["order_id"], name: "index_line_items_on_order_id", using: :btree
+  add_index "line_items", ["product_id"], name: "index_line_items_on_product_id", using: :btree
+
+  create_table "orders", force: true do |t|
+    t.string   "estado_pedido"
+    t.string   "medio_pago"
+    t.string   "direccion_pedido"
+    t.string   "ciudad_envio"
+    t.string   "nombre_apellidos"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
@@ -21,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140304113258) do
     t.integer  "quantity"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "categoria"
   end
 
 end
